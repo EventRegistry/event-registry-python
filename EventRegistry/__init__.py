da9a5919c78c6bdc52f06b8aaf41cc3efb535850
Gregor Leban <gleban@gmail.com>
2015-08-18 22:31:27 +0200

    moved event registry python library to a new repo - made a package

---
+++ EventRegistry/__init__.py
@@ -0,0 +1,9 @@
+
+from .Base import *
+from .EventRegistry import *
+from .ReturnInfo import *
+from .QueryEvents import *
+from .QueryEvent import *
+from .QueryArticles import *
+from .QueryArticle import *
+from .Trends import *
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='EventRegistry',
+      version='6.0',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Gregor Leban',
+      author_email='gleban@gmail.com',
+      license='MIT',
+      packages=['EventRegistry'],
+      zip_safe=False)
