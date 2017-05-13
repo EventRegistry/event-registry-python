7875f8e123f96d9667c53b1dea22e6a2b9f63382
Gregor Leban <gleban@gmail.com>
2017-05-13 23:51:17 +0200

    updated the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='eventregistry',
-      version='6.4.2',
+      version='6.4.3',
       description = "A package that can be used to query information in Event Registry (http://eventregistry.org/)",
       classifiers=[
         'Development Status :: 4 - Beta',
